ae58ceb3069f47ef3be322db7fd55655185f508f
takahashirenya <132458349+takahashirenya@users.noreply.github.com>
2026-04-26 00:47:32 +0900

    2026/04/26

--- STEERING.c
+++ STEERING.c
@@ -8,46 +8,46 @@
 
 // 本番用のコード
 
-// int main(void)
-// {
-//     stdio_init_all();
-//     sleep_ms(2000); // USBシリアル安定待ち
+int main(void)
+{
+    stdio_init_all();
+    sleep_ms(2000); // USBシリアル安定待ち
 
-//     // ボタン状態管理初期化
-//     button_state_init();
-//     button_init();
+    // ボタン状態管理初期化
+    button_state_init();
+    button_init();
 
-//     // ハッチ・ギアのタイマー初期化
-//     hatch_gear_timer_init();
+    // ハッチ・ギアのタイマー初期化
+    hatch_gear_timer_init();
 
-//     // 尾翼サーボ初期化
-//     tail_setup();
+    // 尾翼サーボ初期化
+    tail_setup();
 
-//     hatch_gear_pwm_init();
+    hatch_gear_pwm_init();
 
-//     while (true) {
-//         uint32_t now = now_ms();
+    while (true) {
+        uint32_t now = now_ms();
 
-//         // プルアップ入力なので、押したときLOWなら反転する
-//         bool button_now = (gpio_get(HATCH_GEAR_BUTTON_GPIO) == 0);
+        // プルアップ入力なので、押したときLOWなら反転する
+        bool button_now = (gpio_get(HATCH_GEAR_BUTTON_GPIO) == 0);
 
-//         // ボタン状態更新
-//         button_state_update(button_now, now);
+        // ボタン状態更新
+        button_state_update(button_now, now);
 
-//         // ハッチ・ギアの時系列制御更新
-//         hatch_gear_timer_update();
+        // ハッチ・ギアの時系列制御更新
+        hatch_gear_timer_update();
 
-//         // ハッチ・ギア出力反映
-//         hatch_gear_controller();
+        // ハッチ・ギア出力反映
+        hatch_gear_controller();
 
-//         // 尾翼制御
-//         tail_controller();
+        // 尾翼制御
+        tail_controller();
 
-//         sleep_ms(10);
-//     }
+        sleep_ms(10);
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 
@@ -76,14 +76,16 @@
 //     return 0;
 // }
 
-int main(void)
-{
-    stdio_init_all();
-    sleep_ms(2000); // USBシリアル安定待ち
+// ADCリーダー用コード
 
-    printf("System start\n");
+// int main(void)
+// {
+//     stdio_init_all();
+//     sleep_ms(2000); // USBシリアル安定待ち
 
-    adc_reader();
+//     printf("System start\n");
 
-    return 0;
-}
+//     adc_reader();
+
+//     return 0;
+// }
