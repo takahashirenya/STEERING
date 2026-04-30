69bdfa235cf5fea74955c597a6ddb9891fcd70b5
takahashirenya <132458349+takahashirenya@users.noreply.github.com>
2026-04-30 19:11:05 +0900

    2026/04/30

--- lib/servo/servo.h
+++ lib/servo/servo.h
@@ -18,31 +18,31 @@
 
 
 // tail property
-#define LAD_ADC 47
-#define ELE_ADC 46
+#define LAD_ADC 46
+#define ELE_ADC 47
 
-#define LAD_ADC_CHANNEL 7
-#define ELE_ADC_CHANNEL 6
+#define LAD_ADC_CHANNEL 6
+#define ELE_ADC_CHANNEL 7
 #define LAD_PWM 22
 #define ELE_PWM 28
 
 #define LAD_DEADZONE 100 //ニュートラルの±200の範囲は無視する。
 #define ELE_DEADZONE 300 //ニュートラルの±200の範囲は無視する。
 
-#define ELE_ADC_MAX 3780
+#define ELE_ADC_MAX 3600
 #define ELE_ADC_NUTRAL 2450
 #define ELE_ADC_MIN 1110
 
-#define LAD_ADC_MAX 4060
+#define LAD_ADC_MAX 3300
 #define LAD_ADC_NUTRAL 2040
-#define LAD_ADC_MIN 10
+#define LAD_ADC_MIN 800
 
 #define ELE_REVERSAL_FLAG false
 #define ELE_MAX 2129
 #define ELE_NUTRAL 1655
-#define ELE_MIN 1135
+#define ELE_MIN 1000
 
-#define LAD_REVERSAL_FLAG false
+#define LAD_REVERSAL_FLAG true
 #define LAD_MAX 1834
 #define LAD_NUTRAL 1481
 #define LAD_MIN 1148
--- lib/servo/servo_adc.c
+++ lib/servo/servo_adc.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define ADC_HISTORY_SIZE 2
+#define ADC_HISTORY_SIZE 1
 #define ADC_INPUT_COUNT 8
 
 static uint16_t adc_history[ADC_INPUT_COUNT][ADC_HISTORY_SIZE] = {0};
@@ -27,17 +27,18 @@ uint16_t servo_adc_read_avg(uint8_t adc_input)
 {
     uint16_t new_value = servo_adc_read_raw(adc_input);
 
-    if (adc_count[adc_input] < ADC_HISTORY_SIZE) {
-        adc_history[adc_input][adc_index[adc_input]] = new_value;
-        adc_sum[adc_input] += new_value;
-        adc_count[adc_input]++;
-    } else {
-        adc_sum[adc_input] -= adc_history[adc_input][adc_index[adc_input]];
-        adc_history[adc_input][adc_index[adc_input]] = new_value;
-        adc_sum[adc_input] += new_value;
-    }
+    // if (adc_count[adc_input] < ADC_HISTORY_SIZE) {
+    //     adc_history[adc_input][adc_index[adc_input]] = new_value;
+    //     adc_sum[adc_input] += new_value;
+    //     adc_count[adc_input]++;
+    // } else {
+    //     adc_sum[adc_input] -= adc_history[adc_input][adc_index[adc_input]];
+    //     adc_history[adc_input][adc_index[adc_input]] = new_value;
+    //     adc_sum[adc_input] += new_value;
+    // }
 
-    adc_index[adc_input] = (adc_index[adc_input] + 1) % ADC_HISTORY_SIZE;
+    // adc_index[adc_input] = (adc_index[adc_input] + 1) % ADC_HISTORY_SIZE;
 
-    return (uint16_t)(adc_sum[adc_input] / adc_count[adc_input]);
+    // return (uint16_t)(adc_sum[adc_input] / adc_count[adc_input]);
+    return new_value;
 }
